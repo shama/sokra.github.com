69ea3b50ab53fecc84ed1fc30271322b6a666263
Tobias Koppers <tobias.koppers@googlemail.com>
2012-05-14 23:56:07 +0200

    optimize for minimize

--- web_modules/mybootstrap/bootstrap.js
+++ web_modules/mybootstrap/bootstrap.js
@@ -1,14 +1,14 @@
 // Comment unrequired stuff
 
-// require("script!bootstrap/js/bootstrap-alert");
-// require("script!bootstrap/js/bootstrap-button");
-// require("script!bootstrap/js/bootstrap-carousel");
-require("script!bootstrap/js/bootstrap-collapse");
-require("script!bootstrap/js/bootstrap-dropdown");
-// require("script!bootstrap/js/bootstrap-modal");
-// require("script!bootstrap/js/bootstrap-tooltip");
-// require("script!bootstrap/js/bootstrap-popover");
-// require("script!bootstrap/js/bootstrap-scrollspy");
-// require("script!bootstrap/js/bootstrap-tab");
-require("script!bootstrap/js/bootstrap-transition");
-// require("script!bootstrap/js/bootstrap-typeahead");
+// require("bootstrap/js/bootstrap-alert");
+// require("bootstrap/js/bootstrap-button");
+// require("bootstrap/js/bootstrap-carousel");
+require("bootstrap/js/bootstrap-collapse");
+require("bootstrap/js/bootstrap-dropdown");
+// require("bootstrap/js/bootstrap-modal");
+// require("bootstrap/js/bootstrap-tooltip");
+// require("bootstrap/js/bootstrap-popover");
+// require("bootstrap/js/bootstrap-scrollspy");
+// require("bootstrap/js/bootstrap-tab");
+require("bootstrap/js/bootstrap-transition");
+// require("bootstrap/js/bootstrap-typeahead");
